386e4f4a0806f030b0a5c4611d9c788be84b573f
gloriatse <gloriatse@192.168.0.103>
2017-02-10 01:17:28 +0800

    .

---
+++ src/main/java/hk/cuhk/cmsc5702/MpfCompareHeroApplication.java
@@ -0,0 +1,12 @@
+package hk.cuhk.cmsc5702;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpfCompareHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpfCompareHeroApplication.class, args);
+	}
+}
---
+++ src/test/java/hk/cuhk/cmsc5702/MpfCompareHeroApplicationTests.java
@@ -0,0 +1,16 @@
+package hk.cuhk.cmsc5702;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MpfCompareHeroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
