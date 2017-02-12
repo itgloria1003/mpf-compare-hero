086dca40f32388490dd0892aef301cb443c4ad4f
kitmingchan911 <Ming@Samsung>
2017-02-12 20:26:53 +0800

    Added class for scraping existing MPF scheme providers
    Investigating why cannot scrape details

---
+++ src/main/java/hk/cuhk/cmsc5702/SponsorDetailRepository.java
@@ -0,0 +1,8 @@
+package hk.cuhk.cmsc5702;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface SponsorDetailRepository extends PagingAndSortingRepository<SponsorDetail, Long> {
+
+	
+}
